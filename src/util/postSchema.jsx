053d6552beb0241d7051c0c0bc54e3f2f0689975
Mahmoud Ali <105460542+mahmoudAliaboElhassan@users.noreply.github.com>
2023-09-29 00:45:26 +0300

    Update postSchema.jsx

--- src/util/postSchema.jsx
+++ src/util/postSchema.jsx
@@ -5,10 +5,10 @@ export const postSchema = Yup.object().shape({
     .required("Title is required")
     .min(3, "Title is Very Short Insert at Least 3 characters")
     .max(30, "Maximum Number of characters is 30 characters")
-    .notOneOf([" "], "Input cannot be spaces only"),
+    .matches("^(?! *$).*$", "Title not be Spaces only"),
 
   description: Yup.string()
     .required("Description is required")
     .max(300, "Description is Very long Insert Maximum 300 characters")
-    .notOneOf([" "], "Input cannot be spaces only"),
+    .matches("^(?! *$).*$", "Description not be Spaces only"),
 });
