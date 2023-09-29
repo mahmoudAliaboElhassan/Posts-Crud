2270b953b02fda3d0b8ba3e2dd7dc67d05c64d7a
Mahmoud Ali <105460542+mahmoudAliaboElhassan@users.noreply.github.com>
2023-09-29 20:03:57 +0300

    Update EditPost.jsx

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -6,7 +6,7 @@ import Loading from "../components/Loading";
 import { Form } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { editPost } from "../RTK/Slices/PostSlice";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import { postSchema } from "../util/postSchema";
 import withGuard from "../util/withGuard";
 import { useTranslation } from "react-i18next";
@@ -34,40 +34,94 @@ const EditPost = () => {
     setSelectedImage(selectedImage);
   };
 
-  const formik = useFormik({
-    initialValues: {
-      title: post?.title,
-      description: post?.description,
-    },
-    validationSchema: postSchema,
-    onSubmit: (values) => {
-      dispatch(
-        editPost({
-          id: post.id,
-          title: values.title,
-          description: values.description,
-          img: selectedImage,
-          post_type_val: selectedType?.value || post?.post_type?.value,
-        })
-      )
-        .unwrap()
-        .then(() => {
-          toast.success(t("post-edit"), {
-            position: "top-right",
-            autoClose: 1000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme,
-          });
-          setTimeout(() => {
-            navigate("/");
-          }, 1000);
-        });
-    },
-  });
+  const form = (formikProps) => (
+    <Form
+      onSubmit={formikProps.handleSubmit}
+      style={{ width: "90%", margin: "auto" }}
+    >
+      <div className="d-flex justify-content-between align-items-center image-select-container">
+        <Form.Group className="custom-select mb-3">
+          <Form.Label
+            className="label-color"
+            htmlFor="select"
+            onClick={() => document.getElementById("select").focus()}
+          >
+            {t("select")}
+          </Form.Label>
+          <Select
+            id="select"
+            options={options.slice(1)}
+            value={
+              selectedType
+                ? { label: t(selectedType.value), value: selectedType.value }
+                : {
+                    value: post?.post_type?.value,
+                    label: t(post?.post_type?.value),
+                  }
+            }
+            onChange={(val) => {
+              val.value === ""
+                ? setSelectedType({ label: t("all"), value: "all" })
+                : setSelectedType(val);
+            }}
+          />
+        </Form.Group>
+
+        <Form.Group className="image">
+          <Form.Label className="label-color" htmlFor="image">
+            {t("image-selected")}
+          </Form.Label>
+          <Form.Control
+            type="file"
+            accept="image/*"
+            onChange={handleImage}
+            name="image"
+            value={formikProps.values.image}
+            id="image"
+            className="mb-2"
+          />
+        </Form.Group>
+      </div>
+      <Form.Group className="mb-4" controlId="exampleForm.ControlInput1">
+        <Form.Label className="label-color">{t("title")}</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          onChange={formikProps.handleChange}
+          onBlur={formikProps.handleBlur}
+          value={formikProps.values.title}
+          isInvalid={!!formikProps.errors.title}
+        />
+        <Form.Control.Feedback type="invalid">
+          {formikProps.errors.title}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <ToastContainer />
+      <Form.Group className="mb-4">
+        <Form.Label className="label-color">{t("description")}</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          name="description"
+          onChange={formikProps.handleChange}
+          onBlur={formikProps.handleBlur}
+          value={formikProps.values.description}
+          isInvalid={!!formikProps.errors.description}
+        />
+        <Form.Control.Feedback type="invalid">
+          {formikProps.errors.description}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Loading loading={loading} error={error}>
+        <button className={styles.btnShine} type="submit">
+          {" "}
+          <Edit />
+          <span className="mx-1"> {t("edit")}</span>
+        </button>
+      </Loading>
+    </Form>
+  );
 
   return (
     <motion.div
@@ -76,95 +130,41 @@ const EditPost = () => {
       transition={{ duration: "0.3" }}
     >
       <Loading loading={loading} error={error}>
-        <Form
-          onSubmit={formik.handleSubmit}
-          style={{ width: "90%", margin: "auto" }}
-        >
-          <div className="d-flex justify-content-between align-items-center image-select-container">
-            <Form.Group className="custom-select mb-3">
-              <Form.Label
-                className="label-color"
-                htmlFor="select"
-                onClick={() => document.getElementById("select").focus()}
-              >
-                {t("select")}
-              </Form.Label>
-              <Select
-                id="select"
-                options={options.slice(1)}
-                value={
-                  selectedType
-                    ? {
-                        label: t(selectedType.value),
-                        value: selectedType.value,
-                      }
-                    : {
-                        value: post?.post_type?.value,
-                        label: t(post?.post_type?.value),
-                      }
-                }
-                onChange={(val) => {
-                  val.value === ""
-                    ? setSelectedType({ label: t("all"), value: "all" })
-                    : setSelectedType(val);
-                }}
-              />
-            </Form.Group>
-
-            <Form.Group className="image">
-              <Form.Label className="label-color" htmlFor="image">
-                {t("image-selected")}
-              </Form.Label>
-              <Form.Control
-                type="file"
-                accept="image/*"
-                onChange={handleImage}
-                name="image"
-                value={formik.values.image}
-                id="image"
-                className="mb-2"
-              />
-            </Form.Group>
-          </div>
-          <Form.Group className="mb-4" controlId="exampleForm.ControlInput1">
-            <Form.Label className="label-color">{t("title")}</Form.Label>
-            <Form.Control
-              type="text"
-              name="title"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.title}
-              isInvalid={!!formik.errors.title}
-            />
-            <Form.Control.Feedback type="invalid">
-              {formik.errors.title}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <ToastContainer />
-          <Form.Group className="mb-4">
-            <Form.Label className="label-color">{t("description")}</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              name="description"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.description}
-              isInvalid={!!formik.errors.description}
-            />
-            <Form.Control.Feedback type="invalid">
-              {formik.errors.description}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Loading loading={loading} error={error}>
-            <button className={styles.btnShine} type="submit">
-              {" "}
-              <Edit />
-              <span className="mx-1"> {t("edit")}</span>
-            </button>
-          </Loading>
-        </Form>
+        <Formik
+          initialValues={{
+            title: post?.title,
+            description: post?.description,
+          }}
+          onSubmit={(values) => {
+            dispatch(
+              editPost({
+                id: post.id,
+                title: values.title,
+                description: values.description,
+                img: selectedImage,
+                post_type_val: selectedType?.value || post?.post_type?.value,
+              })
+            )
+              .unwrap()
+              .then(() => {
+                toast.success(t("post-edit"), {
+                  position: "top-right",
+                  autoClose: 1000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme,
+                });
+                setTimeout(() => {
+                  navigate("/");
+                }, 1000);
+              });
+          }}
+          render={form}
+          validationSchema={postSchema}
+        />
       </Loading>
     </motion.div>
   );
