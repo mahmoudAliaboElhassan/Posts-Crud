22d71cd402706e2dff9558bf5e265bc78f391396
Mahmoud Ali <105460542+mahmoudAliaboElhassan@users.noreply.github.com>
2023-09-29 19:48:30 +0300

    Update LoginPage.jsx

--- src/authentication/LoginPage.jsx
+++ src/authentication/LoginPage.jsx
@@ -1,7 +1,6 @@
-import React, { Fragment } from "react";
 import { Container, Form, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import "./formStyle.css";
 import { useTranslation } from "react-i18next";
@@ -32,7 +31,6 @@ const LoginPage = () => {
 
   let errorMsg;
   const { error, loading } = useSelector((state) => state.auth);
-  console.log(error);
   if (error === "Network Error") {
     errorMsg = t("network-error");
   } else if (
@@ -42,102 +40,99 @@ const LoginPage = () => {
     errorMsg = t("login-error");
   } else errorMsg = error;
 
-  const form = (formikProps) => (
-    <Fragment>
-      <Form onSubmit={formikProps.handleSubmit} className="form-style">
-        <Form.Group className="mb-4">
-          <Form.Label className="label-color" htmlFor="email-field">
-            {t("email")}
-          </Form.Label>
-          <ToastContainer />
-          <Form.Control
-            className="input-field"
-            type="email"
-            name="email"
-            placeholder="name@example.com"
-            onChange={formikProps.handleChange}
-            onBlur={formikProps.handleBlur}
-            value={formikProps.values.email}
-            isInvalid={!!formikProps.errors.email}
-            id="email-field"
-          />
-          <Form.Control.Feedback type="invalid">
-            {formikProps.errors.email}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group className="mb-4">
-          <Form.Label className="label-color" htmlFor="password-field">
-            {t("password")}
-          </Form.Label>
-          <Form.Control
-            className="input-field"
-            type="password"
-            placeholder="Enter Password Here"
-            name="password"
-            onChange={formikProps.handleChange}
-            onBlur={formikProps.handleBlur}
-            value={formikProps.values.password}
-            isInvalid={!!formikProps.errors.password}
-            id="password-field"
-          />
-          <Form.Control.Feedback type="invalid">
-            {formikProps.errors.password}
-          </Form.Control.Feedback>
-        </Form.Group>
-        <button type="submit" class="my-2 btn btn-primary" disabled={loading}>
-          {loading ? t("loading") : t("login")}
-        </button>
-        <div>
-          {t("no-account")} <Link to={"/signup"}>{t("signup")}</Link>
-        </div>{" "}
-        <div>
-          <Link to={"/forgetpassword"}> {t("forget-password")} </Link>
-        </div>
-      </Form>
-    </Fragment>
-  );
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    onSubmit: (values) => {
+      console.log(values.email);
+      dispatch(
+        Login({
+          email: values.email,
+          password: values.password,
+        })
+      )
+        .unwrap()
+        .then(() => {
+          toast.success(t("login-success"), {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme,
+          });
+          setTimeout(() => {
+            navigate("/");
+          }, 1000);
+        })
+        .catch(() => {
+          swal({
+            icon: "error",
+            title: t("error"),
+            text: `${errorMsg}`,
+          });
+        });
+    },
+    validationSchema: authSchema,
+  });
   return (
     <Container>
       <Col xs={{ span: 8, offset: 2 }}>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-          }}
-          render={form}
-          onSubmit={(values) => {
-            dispatch(
-              Login({
-                email: values.email,
-                password: values.password,
-              })
-            )
-              .unwrap()
-              .then(() => {
-                toast.success(t("login-success"), {
-                  position: "top-right",
-                  autoClose: 1000,
-                  hideProgressBar: false,
-                  closeOnClick: true,
-                  pauseOnHover: true,
-                  draggable: true,
-                  progress: undefined,
-                  theme,
-                });
-                setTimeout(() => {
-                  navigate("/");
-                }, 1000);
-              })
-              .catch(() => {
-                swal({
-                  icon: "error",
-                  title: t("error"),
-                  text: `${errorMsg}`,
-                });
-              });
-          }}
-          validationSchema={authSchema}
-        />
+        <Form onSubmit={formik.handleSubmit} className="form-style">
+          <Form.Group className="mb-4">
+            <Form.Label className="label-color" htmlFor="email-field">
+              {t("email")}
+            </Form.Label>
+            <ToastContainer />
+            <Form.Control
+              className="input-field"
+              type="email"
+              name="email"
+              placeholder="name@example.com"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              isInvalid={!!formik.errors.email}
+              id="email-field"
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-4">
+            <Form.Label className="label-color" htmlFor="password-field">
+              {t("password")}
+            </Form.Label>
+            <Form.Control
+              className="input-field"
+              type="password"
+              placeholder={t("password-here")}
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              isInvalid={!!formik.errors.password}
+              id="password-field"
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.password}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <button type="submit" class="my-2 btn btn-primary" disabled={loading}>
+            {loading ? t("loading") : t("login")}
+          </button>
+          <div>
+            {t("no-account")} <Link to={"/signup"}>{t("signup")}</Link>
+          </div>{" "}
+          <div>
+            <Link to={"/forgetpassword"}> {t("forget-password")} </Link>
+          </div>
+        </Form>
       </Col>
     </Container>
   );
