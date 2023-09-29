24bef3583af41ec04777030daf3c16fdc668d226
Mahmoud Ali <105460542+mahmoudAliaboElhassan@users.noreply.github.com>
2023-09-29 19:47:46 +0300

    Update ForgotPassord.jsx

--- src/authentication/ForgotPassord.jsx
+++ src/authentication/ForgotPassord.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { Button, Container, Form, Col } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import swal from "sweetalert";
 import "./formStyle.css";
@@ -28,80 +27,77 @@ const ForgotPassword = () => {
     errorMsg = t("network-error");
   } else errorMsg = error;
 
-  const form = (formikProps) => (
-    <Form onSubmit={formikProps.handleSubmit} className="form-style">
-      <Form.Group>
-        <Form.Label className="label-color" htmlFor="email-field">
-          {t("email")}
-        </Form.Label>{" "}
-        <ToastContainer />
-        <Form.Control
-          type="email"
-          name="email"
-          id="email-field"
-          placeholder="name@example.com"
-          onChange={formikProps.handleChange}
-          onBlur={formikProps.handleBlur}
-          value={formikProps.values.email}
-          isInvalid={!!formikProps.errors.email}
-        />
-        <Form.Control.Feedback type="invalid">
-          {formikProps.errors.email}
-        </Form.Control.Feedback>
-      </Form.Group>
-
-      <Button type="submit" className="mt-3" disabled={loading}>
-        {loading ? t("loading") : t("send-password")}
-      </Button>
-
-      <div className="ms-1 me-1 mt-1">
-        <Link to={"/login"}> {t("back")}</Link>
-      </div>
-    </Form>
-  );
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: (values) => {
+      dispatch(
+        ForgetPassword({
+          email: values.email,
+        })
+      )
+        .unwrap()
+        .then(() => {
+          toast.success(t("forget-message"), {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme,
+          });
+          setTimeout(() => {
+            navigate("/login");
+          }, 1000);
+        })
+        .catch(() => {
+          swal({
+            icon: "error",
+            title: t("error"),
+            text: `${errorMsg}`,
+          });
+        });
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().email().required("Email Field is required"),
+    }),
+  });
 
   return (
     <Container>
       <Col xs={{ span: 8, offset: 2 }}>
-        <Formik
-          initialValues={{
-            email: "",
-          }}
-          render={form}
-          onSubmit={(values) => {
-            dispatch(
-              ForgetPassword({
-                email: values.email,
-              })
-            )
-              .unwrap()
-              .then(() => {
-                toast.success(t("forget-message"), {
-                  position: "top-right",
-                  autoClose: 1000,
-                  hideProgressBar: false,
-                  closeOnClick: true,
-                  pauseOnHover: true,
-                  draggable: true,
-                  progress: undefined,
-                  theme,
-                });
-                setTimeout(() => {
-                  navigate("/login");
-                }, 1000);
-              })
-              .catch(() => {
-                swal({
-                  icon: "error",
-                  title: t("error"),
-                  text: `${errorMsg}`,
-                });
-              });
-          }}
-          validationSchema={Yup.object().shape({
-            email: Yup.string().email().required("Email Field is required"),
-          })}
-        />
+        <Form onSubmit={formik.handleSubmit} className="form-style">
+          <Form.Group>
+            <Form.Label className="label-color" htmlFor="email-field">
+              {t("email")}
+            </Form.Label>{" "}
+            <ToastContainer />
+            <Form.Control
+              type="email"
+              name="email"
+              id="email-field"
+              placeholder="name@example.com"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              isInvalid={!!formik.errors.email}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Button type="submit" className="mt-3" disabled={loading}>
+            {loading ? t("loading") : t("send-password")}
+          </Button>
+
+          <div className="ms-1 me-1 mt-1">
+            <Link to={"/login"}> {t("back")}</Link>
+          </div>
+        </Form>
       </Col>
     </Container>
   );
