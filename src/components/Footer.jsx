5ea0b1639f12c9b0ba52f1cea674cf268c78be88
Mahmoud Ali <105460542+mahmoudAliaboElhassan@users.noreply.github.com>
2023-09-29 21:20:25 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import "./Header.css";
 import "./Footer.css";
 import { ReactComponent as Heart } from "../assets/heart.svg";
